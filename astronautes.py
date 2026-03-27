703c6c36fd8df5d727aeceb99baa87f868d9da4e
levraytao <levraytao@gmail.com>
2026-03-27 11:07:07 +0100

    depuis .dev

--- astronautes.py
+++ astronautes.py
@@ -1,6 +1,7 @@
 import requests
 
 contenu = requests.get("http://api.open-notify.org/astros.json")
+print(contenu.json())
 for personne in contenu.json()["people"]:
     if personne["craft"] == "ISS":
         print(personne)
